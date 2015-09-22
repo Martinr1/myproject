f6d04a5d88b1d8a6893064a3476f1b53081023cc
Martin Ristov <martinristov1@gmail.com>
2015-09-22 14:36:51 -0400

    tableupdate

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,7 @@ require('./controllers/_index');
 require('./services/_index');
 require('./directives/_index');
 
+
 // create and bootstrap application
 angular.element(document).ready(function() {
 
@@ -30,20 +31,5 @@ angular.element(document).ready(function() {
   angular.module('app').run(require('./on_run'));
 
   angular.bootstrap(document, ['app']);
-
-  app.controller("MainController", function(){
-
-   this.groups =  [
-    {frequency: 'monthly', name: 'Hero', price: 25.0},
-    {frequency: 'monthly', name: 'Leader', price: 50.0},
-    {frequency: 'monthly', name: 'Champion', price: 100.0},
-    {frequency: 'once', name: 'Daisy', price: 2.0},
-    {frequency: 'once', name: 'Butterfly', price: 10.0},
-    {frequency: 'once', name: 'Honey Bee', price: 25.0},
-    {frequency: 'once', name: 'Hummingbird', price: 50.0}
-];
-
-
-  });
 
 });
